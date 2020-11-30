22355f77f427e41c2fe9ef0fd482270b42769a83
gonpeche <espeche.g@gmail.com>
2020-11-30 10:31:15 -0300

    feat: type Number

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('bam');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+add(number1, number2);
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('bam');
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+add(number1, number2);
